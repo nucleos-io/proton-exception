2b7d0f89e0b7a3cac314e15275301a2bf84770dd
luish000 <luish000.lh@gmail.com>
2016-02-23 15:05:21 -0430

    push commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports = class extends Error {
+
+  constructor(message) {
+    super(message)
+    this.name = this.constructor.name
+    this.message = message
+    this._bindToApp()
+    this.expose()
+    Error.captureStackTrace(this, this.constructor.name)
+  }
+
+  handle(ctx) {
+    return ctx.status = 500
+  }
+
+  expose() {
+    global[this.name] = this.model
+    return true
+  }
+
+  _bindToApp() {
+    this.proton.app.exceptions[this.name] = this
+  }
+
+  /**
+   * @return class name
+   */
+  get name() {
+    return this.constructor.name
+  }
+
+}

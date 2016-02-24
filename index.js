69557ca9d9f5d4647def0ded70cbaf2dc575bcd7
luish000 <luish000.lh@gmail.com>
2016-02-24 13:32:38 -0430

    remove the proton object and proton methods in exception class

--- index.js
+++ index.js
@@ -2,26 +2,14 @@
 
 module.exports = class Exception extends Error {
 
-  constructor(message, proton) {
+  constructor(message) {
     super(message)
-    this.proton = proton
     this.message = message
-    this._bindToApp()
-    this.expose()
     Error.captureStackTrace(this, this.constructor.name)
   }
 
   handle(ctx) {
     return ctx.status = 500
-  }
-
-  expose() {
-    global[this.name] = this.constructor
-    return true
-  }
-
-  _bindToApp() {
-    this.proton.app.exceptions[this.name] = this.constructor
   }
 
   /**
